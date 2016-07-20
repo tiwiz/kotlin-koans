2f8bf2688f4f97c59f8b49e7abedbe7b097ca9a2
Roberto Orgiu <roberto.orgiu@gmail.com>
2016-07-20 13:12:29 +0200

    Completes task17

--- src/ii_collections/_17_MaxMin.kt
+++ src/ii_collections/_17_MaxMin.kt
@@ -5,12 +5,9 @@ fun example4() {
     val longestString = listOf("a", "b").maxBy { it.length }
 }
 
-fun Shop.getCustomerWithMaximumNumberOfOrders(): Customer? {
-    // Return a customer whose order count is the highest among all customers
-    todoCollectionTask()
-}
+fun Shop.getCustomerWithMaximumNumberOfOrders(): Customer? = customers.maxBy { it.orders.size }
 
 fun Customer.getMostExpensiveOrderedProduct(): Product? {
-    // Return the most expensive product which has been ordered
-    todoCollectionTask()
+
+    return orderedProducts.maxBy { it.price }
 }
